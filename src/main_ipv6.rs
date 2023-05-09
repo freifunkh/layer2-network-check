fda5d844fd1aa1a8e7ef710cd5f9d2ab4ce5b053
lemoer <git@irrelefant.net>
2023-05-09 02:15:00 +0200

    Do icmpv6 ping with the retrieved config

--- src/main_ipv6.rs
+++ src/main_ipv6.rs
@@ -1,12 +1,14 @@
 #![allow(clippy::option_map_unit_fn)]
 mod utils;
 
+use std::cmp;
+use std::collections::HashMap;
 use std::os::unix::io::AsRawFd;
 
 use smoltcp::iface::{Config, Interface, SocketSet};
 use smoltcp::socket::dhcpv4;
 use smoltcp::time::Instant;
-use smoltcp::wire::{EthernetAddress, IpCidr, Ipv6Address, IpAddress, NdiscPrefixInformation};
+use smoltcp::wire::{EthernetAddress, IpCidr, Ipv6Address, IpAddress, NdiscPrefixInformation, Icmpv6Repr};
 use smoltcp::{
     phy::{wait as phy_wait, Device, Medium, RawSocket},
     time::Duration,
@@ -16,6 +18,44 @@ use smoltcp::socket::icmp;
 use smoltcp::wire::{
     Icmpv6Packet, NdiscRepr, Ipv6Cidr
 };
+
+use byteorder::{ByteOrder, NetworkEndian};
+
+macro_rules! send_icmp_ping {
+    ( $repr_type:ident, $packet_type:ident, $ident:expr, $seq_no:expr,
+      $echo_payload:expr, $socket:expr, $remote_addr:expr ) => {{
+        let icmp_repr = $repr_type::EchoRequest {
+            ident: $ident,
+            seq_no: $seq_no,
+            data: &$echo_payload,
+        };
+
+        let icmp_payload = $socket.send(icmp_repr.buffer_len(), $remote_addr).unwrap();
+
+        let icmp_packet = $packet_type::new_unchecked(icmp_payload);
+        (icmp_repr, icmp_packet)
+    }};
+}
+
+macro_rules! get_icmp_pong {
+    ( $repr_type:ident, $repr:expr, $payload:expr, $waiting_queue:expr, $remote_addr:expr,
+      $timestamp:expr, $received:expr ) => {{
+        if let $repr_type::EchoReply { seq_no, data, .. } = $repr {
+            if let Some(_) = $waiting_queue.get(&seq_no) {
+                let packet_timestamp_ms = NetworkEndian::read_i64(data);
+                println!(
+                    "{} bytes from {}: icmp_seq={}, time={}ms",
+                    data.len(),
+                    $remote_addr,
+                    seq_no,
+                    $timestamp.total_millis() - packet_timestamp_ms
+                );
+                $waiting_queue.remove(&seq_no);
+                $received += 1;
+            }
+        }
+    }};
+}
 
 
 const EUI64_MIDDLE_VALUE: [u8; 2] = [0xff, 0xfe];
@@ -242,12 +282,127 @@ fn main() {
     set_ipv6_addr(&mut iface, selected_ip.unwrap());
 
     iface.routes_mut().add_default_ipv6_route(selected_router.unwrap()).unwrap();
+
+    // Create sockets
+    let icmp_rx_buffer = icmp::PacketBuffer::new(vec![icmp::PacketMetadata::EMPTY], vec![0; 256]);
+    let icmp_tx_buffer = icmp::PacketBuffer::new(vec![icmp::PacketMetadata::EMPTY], vec![0; 256]);
+    let icmp_socket = icmp::Socket::new(icmp_rx_buffer, icmp_tx_buffer);
+    let icmp_handle = sockets.add(icmp_socket);
+
+    let mut send_at = Instant::from_millis(0);
+    let mut seq_no = 0;
+    let mut received = 0;
+    let mut echo_payload = [0xffu8; 40];
+    let mut waiting_queue = HashMap::new();
+    let ident = 0x22b;
+
+    let remote_addr = IpAddress::v6(0x2001, 0x4860, 0x4860, 0, 0, 0, 0, 0x8844); // from("2001:4860:4860:0:0:0:0:8844");
+
+    let count = 4;
+    let timeout = Duration::from_secs(1);
+    let interval = Duration::from_secs(1);
+    let device_caps = device.capabilities();
+
+    loop {
+        let timestamp = Instant::now();
+        iface.poll(timestamp, &mut device, &mut sockets);
+
+        let timestamp = Instant::now();
+        let socket = sockets.get_mut::<icmp::Socket>(icmp_handle);
+        if !socket.is_open() {
+            socket.bind(icmp::Endpoint::Ident(ident)).unwrap();
+            send_at = timestamp;
+        }
+
+        if socket.can_send() && seq_no < count as u16 && send_at <= timestamp {
+            NetworkEndian::write_i64(&mut echo_payload, timestamp.total_millis());
+
+            let (icmp_repr, mut icmp_packet) = send_icmp_ping!(
+                Icmpv6Repr,
+                Icmpv6Packet,
+                ident,
+                seq_no,
+                echo_payload,
+                socket,
+                remote_addr
+            );
+            icmp_repr.emit(
+                &iface.ipv6_addr().unwrap().into_address(),
+                &remote_addr,
+                &mut icmp_packet,
+                &device_caps.checksum,
+            );
+
+            waiting_queue.insert(seq_no, timestamp);
+            seq_no += 1;
+            send_at += interval;
+        }
+
+        if socket.can_recv() {
+            let (payload, _) = socket.recv().unwrap();
+
+            let icmp_packet = Icmpv6Packet::new_checked(&payload).unwrap();
+            let icmp_repr = Icmpv6Repr::parse(
+                &remote_addr,
+                &iface.ipv6_addr().unwrap().into_address(),
+                &icmp_packet,
+                &device_caps.checksum,
+            ).unwrap();
+            get_icmp_pong!(
+                Icmpv6Repr,
+                icmp_repr,
+                payload,
+                waiting_queue,
+                remote_addr,
+                timestamp,
+                received
+            );
+        }
+
+        waiting_queue.retain(|seq, from| {
+            if timestamp - *from < timeout {
+                true
+            } else {
+                println!("From {remote_addr} icmp_seq={seq} timeout");
+                false
+            }
+        });
+
+        if seq_no == count as u16 && waiting_queue.is_empty() {
+            break;
+        }
+
+        let timestamp = Instant::now();
+        match iface.poll_at(timestamp, &sockets) {
+            Some(poll_at) if timestamp < poll_at => {
+                let resume_at = cmp::min(poll_at, send_at);
+                phy_wait(fd, Some(resume_at - timestamp)).expect("wait error");
+            }
+            Some(_) => (),
+            None => {
+                phy_wait(fd, Some(send_at - timestamp)).expect("wait error");
+            }
+        }
+    }
+
+    println!("--- {remote_addr} ping statistics ---");
+    println!(
+        "{} packets transmitted, {} received, {:.0}% packet loss",
+        seq_no,
+        received,
+        100.0 * (seq_no - received) as f64 / seq_no as f64
+    );
 }
 
 fn set_ipv6_addr(iface: &mut Interface, cidr: Ipv6Cidr) {
     iface.update_ip_addrs(|addrs| {
         if cidr.address() == Ipv6Address::UNSPECIFIED {
             return;
+        }
+
+        let old_addr = addrs.pop();
+        if let Some(addr) = old_addr {
+            println!("deconfigured IP {}", addr);
         }
 
         addrs
